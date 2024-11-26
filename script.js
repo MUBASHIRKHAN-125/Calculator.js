1e87046e97c3e2342c89911189332778ce6b7ce3
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2024-11-26 14:42:21 +0500

    working

---
+++ script.js
@@ -0,0 +1,30 @@
+function Positive()
+{
+let Sum = Number (document.getElementById("Sum").value)
+let Minus = Number (document.getElementById("Minus").value)
+document.getElementById("containernumber").innerText = Sum + Minus;
+}
+function Negative()
+{
+let Sum = Number (document.getElementById("Sum").value)
+let Minus = Number (document.getElementById("Minus").value)
+document.getElementById("containernumber").innerText = Sum - Minus;
+}
+function Multiply()
+{
+let Sum = Number (document.getElementById("Sum").value)
+let Minus = Number (document.getElementById("Minus").value)
+document.getElementById("containernumber").innerText = Sum * Minus;
+}
+function Divide()
+{
+let Sum = Number (document.getElementById("Sum").value)
+let Minus = Number (document.getElementById("Minus").value)
+document.getElementById("containernumber").innerText = Sum / Minus;
+}
+// function reset()
+// {
+// let Sum = Number (document.getElementById("Sum").value)
+// let Minus = Number (document.getElementById("Minus").value)
+// document.getElementById("containernumber").innerText = Sum reset Minus;
+// }
